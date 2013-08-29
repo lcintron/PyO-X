2f41380747d49b6d7ec689bb7f69d87c19a0b2e3
luis cintron <l.cintron@live.com>
2013-08-29 18:46:14 -0400

    bunch of new files. Adding gauges to the main interface and removing use of labels to display data.

--- PyGauge.py
+++ PyGauge.py
@@ -1,7 +1,7 @@
 import sys
 import time
 from PyQt4 import QtGui, QtCore, Qt
-import math
+from GaugeWidget import GaugeWidget
 
 class pygauge_thread(QtCore.QThread):
 	def __init__(self, gauges):
@@ -15,7 +15,6 @@ class pygauge_thread(QtCore.QThread):
 				gauge.setValue(x)
 			time.sleep(.05)
 			x = 0 if x ==99 else x+1
-			#print 'Setting ' + str(float(x)/100) + ' and ' + str(x)
 		
  
 class Example(QtGui.QWidget):
@@ -27,18 +26,17 @@ class Example(QtGui.QWidget):
 		self.setWindowTitle('Dial Guage')
 
 		layout = QtGui.QVBoxLayout(self)
-		#lbl = QtGui.QLabel("A Label")
-		#layout.addWidget(lbl)
-
-		self.gauge = GaugeWidget('oil temp',0)
+		self.gauge = GaugeWidget('oiltemp','oil temp',0)
 		self.gauge.setSizePolicy(QtGui.QSizePolicy(
 		QtGui.QSizePolicy.Expanding,
 			QtGui.QSizePolicy.Expanding))
 		layout.addWidget(self.gauge)
-		self.gauge2 = GaugeWidget('mph',0)
+		
+		self.gauge2 = GaugeWidget('mph','mph',0)
 		self.gauge2.setSizePolicy(QtGui.QSizePolicy(
 			QtGui.QSizePolicy.Expanding,
 			QtGui.QSizePolicy.Expanding))
+		
 		self.gauges = [self.gauge, self.gauge2]
 		layout.addWidget(self.gauge2)
 		self.show()
@@ -47,66 +45,7 @@ class Example(QtGui.QWidget):
 		self.pygauge_thread1 = pygauge_thread(self.gauges)
 		self.pygauge_thread1.start()
  
-class GaugeWidget(QtGui.QWidget):
- 
-	def __init__(self,text='', initialValue=0, maxValue=100,   *args, **kwargs):
-		super(GaugeWidget, self).__init__(*args, **kwargs)
-		self._bg = QtGui.QPixmap("speedo4.png")
-		self.maxValue = maxValue
-		self.text = text
-		self.setValue(initialValue)
-        
-	def setValue(self, val):
-		self.raw_value = val
-		val = val/float(self.maxValue)
-		val = float(min(max(val, 0), 1))
-		self._value = -270 * val
-		self.update()
- 
- 	def paintEvent(self, e):
- 		painter = QtGui.QPainter(self)
- 		painter.setRenderHint(painter.Antialiasing)
- 		rect = e.rect()
- 		gauge_rect = QtCore.QRect(rect)
- 		size = gauge_rect.size()
- 		pos = gauge_rect.center()
- 		gauge_rect.moveCenter( QtCore.QPoint(pos.x()-size.width(), pos.y()-size.height()) )
-		gauge_rect.setSize(size*.9)
-		gauge_rect.moveCenter(pos)
 
-		refill_rect = QtCore.QRect(gauge_rect)
-		size = refill_rect.size()
-		pos = refill_rect.center()
-		refill_rect.moveCenter( QtCore.QPoint(pos.x()-size.width(), pos.y()-size.height()) )
-		# smaller than .9 == thicker gauge
-		refill_rect.setSize(size*.9)
-		refill_rect.moveCenter(pos)
-		painter.setPen(QtCore.Qt.NoPen)
-		painter.drawPixmap(rect, self._bg )
-		painter.save()
-		grad = QtGui.QConicalGradient(QtCore.QPointF(gauge_rect.center()), 270.0)
-		grad.setColorAt(.75, QtCore.Qt.green)
-		grad.setColorAt(.5, QtCore.Qt.yellow)
-		grad.setColorAt(.1, QtCore.Qt.red)
-		painter.setBrush(grad)
-		painter.drawPie(gauge_rect, 225.0*16, self._value*16)
-		painter.restore()
-		painter.setBrush(QtGui.QBrush(self._bg.scaled(rect.size())))
-		painter.drawEllipse(refill_rect)
-		
-		painter.setPen(QtGui.QColor(168, 34, 3))
-		fontSize=size.width()*.06
-		gauge_rect.moveCenter(QtCore.QPoint(pos.x(), pos.y()+3*fontSize))
-		painter.setFont(QtGui.QFont('AnyStyle',fontSize))
-		painter.drawText(gauge_rect,QtCore.Qt.AlignCenter, self.text)
-		
-		fontSize=size.width()*.20
-		gauge_rect.moveCenter(QtCore.QPoint(pos.x(), pos.y()-1*fontSize))
-		painter.setFont(QtGui.QFont('AnyStyle',fontSize))
-		painter.drawText(gauge_rect,QtCore.Qt.AlignCenter, str(self.raw_value))
-		
-		painter.end()	
-		super(GaugeWidget,self).paintEvent(e)
        
 def main():
     app = QtGui.QApplication(sys.argv)
--- PyOx_Widget.py
+++ PyOx_Widget.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python
 #-*- coding: utf-8 -*-
 import platform
-import time
 import sys
 import styles
 from pyox_obdthread import pyox_obdthread
@@ -19,37 +18,32 @@ class PyOX_Gui(QtGui.QWidget):
 		#This label is required to be sent to the pyox_obdthread class
 		#as the class makes a default call to it.
 		self.connecting = QtGui.QLabel("Connecting...", self)
-		self.speed = QtGui.QLabel('###', self)
-		self.speedUnit = QtGui.QLabel('mph', self)
-		self.rpm = QtGui.QLabel('####', self)
-		self.rpmUnit = QtGui.QLabel('rpm', self)
+		self.speed = GaugeWidget('speed', 'mph', 0, 190)
+		self.rpm = GaugeWidget('rpm', 'rpm', 0,9000)
+		self.initUI()
 		self.labels_collection = {'speed': self.speed
 								, 'rpm':self.rpm
 								, 'connecting':self.connecting
-								, 'lbl1':self.speedUnit
-								, 'lbl2':self.rpmUnit}
-		self.initUI()
+								}
 	
 	#initialize ui components
 	def initUI(self):
 		uiSizes = QtGui.QDesktopWidget().availableGeometry()
 		self.connecting.setStyleSheet(styles.stylesObd['style2'])
 		self.connecting.move(20, 20)
-		self.speed.setStyleSheet(styles.stylesObd['style1'])
-		self.speed.setScaledContents(True)
-		self.speedUnit.setStyleSheet(styles.stylesObd['style2'])
-		self.speed.move(uiSizes.width()/2-90, uiSizes.height()/2-200)
-		self.speedUnit.move(uiSizes.width()/2+105, uiSizes.height()/2-145)
-		self.rpm.setStyleSheet(styles.stylesObd['style1'])	
-		self.rpm.setScaledContents(True)
-		self.rpmUnit.setStyleSheet(styles.stylesObd['style2'])
-		self.rpm.move(uiSizes.width()/2-140, uiSizes.height()/2-50)	
-		self.rpmUnit.move(uiSizes.width()/2+124, uiSizes.height()/2+5)
+
+		hbox = QtGui.QHBoxLayout()
+		hbox.addWidget(self.speed)
+		hbox.addWidget(self.rpm)
+		vbox = QtGui.QVBoxLayout()
+		vbox.addLayout(hbox)
+		self.setLayout(vbox)
+		
 		palette1 = QtGui.QPalette()
 		palette1.setBrush(self.backgroundRole(), QtGui.QBrush(QtGui.QImage("image.png")))
 		self.setPalette(palette1)
-		#self.showFullScreen()
 		self.setWindowTitle("PyO-X")
+		self.showFullScreen()
 		self.show()
 
 	#creates and starts a new thread that reads the obd2 port and updates the ui
